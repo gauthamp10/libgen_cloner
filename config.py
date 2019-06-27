7af238f608ca362cf2e79a647b350ffda769f1e1
Gautham Prakash <gauthamp10@gmail.com>
2019-06-27 09:52:41 +0530

    added firebase

---
+++ config.py
@@ -0,0 +1,9 @@
+config = {
+    "apiKey": "AIzaSyAZ1MfXDYLgVHXy1y_CxbAWlch9cZiWSP0",
+    "authDomain": "scraper-592bc.firebaseapp.com",
+    "databaseURL": "https://scraper-592bc.firebaseio.com",
+    "storageBucket": "scraper-592bc.appspot.com",
+    "serviceAccount": "",
+    "uname":"elkosmmx@gmail.com",
+    "passw":"www-scraper",
+}
--- test.py
+++ test.py
@@ -1,13 +1,39 @@
 import requests
 import json
 import random
-import itertools
+from bs4 import BeautifulSoup
+from multiprocessing import Pool
 import wget
+import getpass
+import requests
+import pyrebase
+import datetime
+from fire_base import *
+from config import config
 
-def get_book_data(id):
-    data=requests.get('http://libgen.io/json.php?ids='+str(1)+'&fields=*').json()
-    print(data)
-    return data
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+user = auth.sign_in_with_email_and_password(config['uname'],config['passw'])
+root ="Test_Write" 								#Name of firebase db root name
+db = firebase.database()
+print("-----Logged in------\n")
 
-id=random.randrange(999999)
-get_book_data(id)
+ids=list()
+
+def get_book_data(ids):
+    headers = {'User-Agent': 'My User Agent 1.0','From': 'youremail@domain.com'  }
+    data=requests.get('http://libgen.io/json.php?ids='+str(ids)+'&fields=*',headers=headers).json()[0]
+    print("Book"+str(ids)+": ",data['title'])
+    write(db, user, data, ids,"Test_Write")
+    
+def generate_ids():
+    for i  in range(1,999999):
+        ids.append(i)
+    
+generate_ids()
+
+
+p = Pool(20)
+p.map(get_book_data, ids)
+p.terminate()
+p.join()
